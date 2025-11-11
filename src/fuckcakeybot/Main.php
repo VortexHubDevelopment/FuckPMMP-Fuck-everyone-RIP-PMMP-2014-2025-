d2bb6ba4b809a9685ba06537c76db68e78887a70
VortexHub <145719670+VortexHubDevelopment@users.noreply.github.com>
2025-11-10 22:02:07 -0800

    Create Main.php

---
+++ src/fuckcakeybot/Main.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace fuckcakeybot;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase {
+
+    protected function onLoad() : void{
+        $this->getServer()->getLogger()->info("My name is Mr.CakeSlayer and i dictate PMMP discord as my own because no one elae uses my bot ALSO i dont contribute to pmmp only to discord. YES i dont actaully work for PMMP only made a bot no one asked for... I left a note saying if i was silenced its true guess what im making this because they tried silenecing me!");
+    }
+
+    protected function onEnable() : void{
+        $this->getServer()->getLogger()->("You do know that Fate OR Carl bot exists smh no need for this unknown bot who knows it may carru viruses"):
