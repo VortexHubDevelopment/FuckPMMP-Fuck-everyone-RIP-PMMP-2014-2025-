69c21a82b01d38085f3359edb8dcd7eaa54210cd
VortexHub <145719670+VortexHubDevelopment@users.noreply.github.com>
2025-11-10 22:14:00 -0800

    Update Main.php

--- src/fuckcakeybot/Main.php
+++ src/fuckcakeybot/Main.php
@@ -8,6 +8,7 @@ use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase {
 
+    //just leave no one likes u cakey and no this isnt a comeback unless u leave or bring some sort of apeal system... Ur choice...
     protected function onLoad() : void{
         $this->getLogger()->info("My name is Mr.CakeSlayer and i dictate PMMP discord as my own because no one elae uses my bot ALSO i dont contribute to pmmp only to discord. YES i dont actaully work for PMMP only made a bot no one asked for... I left a note saying if i was silenced its true guess what im making this because they tried silenecing me!");
     }
